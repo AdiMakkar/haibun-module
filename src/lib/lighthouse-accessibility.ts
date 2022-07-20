572d88ce6d9930411081b22ce89754fcdee71ab5
Adi Makkar <adimakkar.ca@gmail.com>
2022-07-20 09:57:23 -0400

    accessibility stepper implemented, closes #1

--- src/lib/lighthouse-accessibility.ts
+++ src/lib/lighthouse-accessibility.ts
@@ -3,6 +3,16 @@ import { findStepper } from "@haibun/core/build/lib/util";
 const { playAudit } = require('playwright-lighthouse');
 const playwright = require('playwright');
 
+type TLighthouseReport = {
+    lhr: {
+        audits: {
+            [audit: string]: {
+                score: number
+            }
+        }
+    }
+}
+
 export async function checkAccessibility(uri: String) {
     const browser = await playwright['chromium'].launch({
         args: ['--remote-debugging-port=9171'],
@@ -10,14 +20,20 @@ export async function checkAccessibility(uri: String) {
     const page = await browser.newPage();
     await page.goto(uri);
 
-    const result = await playAudit({ // only accessibility as the metric for performance thresholds
+    const result: TLighthouseReport = await playAudit({ // only accessibility as the metric for performance thresholds
         page,
         thresholds: {
-            accessibility: 90,
+            accessibility: 90, // passing value confirmed > 90
         },
         port: 9171
     });
     await browser.close();
-    
-    return result;
+
+    const scored = Object.entries(result.lhr.audits).filter(([audit, values]) => values.score !== null)
+            .map(([audit, value]) => ({ audit, score: value.score }));
+            console.log(scored);
+            
+        const ok = (scored.every(({ score }) => score > 90));
+
+    return { ok,result };
 };
